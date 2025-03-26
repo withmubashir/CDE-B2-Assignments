eb887161cdc0651eeb8c4f66d8f60715ee0eb4f7
withmubashir <kingmubashir018@gmail.com>
2025-03-26 16:59:10 +0500

    Assignment

--- Assignments 03.py
+++ Assignments 03.py
@@ -1,34 +1,40 @@
-# # Question 1
+# Question 1
 
-name = input("Enter any name: ")
-value = name.lower()
-total_vowels = 0
-for i in value:
-    if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
-        total_vowels += 1
+# name = input("Enter any name: ")
+# value = name.lower()
+# total_vowels = 0
+# for i in value:
+#     if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
+#         total_vowels += 1
 
-print(total_vowels)
+# print(total_vowels)
 
-# Question 2
+# # Question 2
 
-input = input("Enter any string: ")
+# input = input("Enter any string: ")
 
-uppercase = 0
-lowercase = 0
-digits = 0
-whitespace = 0
+# uppercase = 0
+# lowercase = 0
+# digits = 0
+# whitespace = 0
 
-for letter in input:
-    if letter.isupper():
-        uppercase += 1
-    elif letter.islower():
-        lowercase += 1
-    elif letter.isdigit():
-        digits += 1
-    elif letter.isspace():
-        whitespace += 1
+# for letter in input:
+#     if letter.isupper():
+#         uppercase += 1
+#     elif letter.islower():
+#         lowercase += 1
+#     elif letter.isdigit():
+#         digits += 1
+#     elif letter.isspace():
+#         whitespace += 1
 
-print(uppercase)
-print(lowercase)
-print(digits)
-print(whitespace)
+# print(uppercase)
+# print(lowercase)
+# print(digits)
+# print(whitespace)
+
+# Question 3
+
+revers = input("Enter any Input: ")
+new_revers = revers[-1] + revers[1:-1] + revers[0]
+print(new_revers)
